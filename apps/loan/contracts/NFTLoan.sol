5f622d47ea17bf3c6b3ee504335d50fb3f533785
Nwosu-Ihueze <nwosunneomagmail.com>
2022-03-06 16:27:57 -0500

    added frontend

--- apps/loan/contracts/NFTLoan.sol
+++ apps/loan/contracts/NFTLoan.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "utils/ABDKMath64x64.sol";
+import "../utils/ABDKMath64x64.sol";
 
 /**
  * @title NFTLoan: NFT-collateralized lending
